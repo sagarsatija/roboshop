c77b7a2879074d1a4289549e7a9f9db20bae1f10
SagarSatija <satijasagar14@gmail.com>
2019-09-27 14:45:06 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule} from '@angular/router'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from 'angularfire2'
@@ -13,6 +13,11 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,14 +25,50 @@ import { CheckoutComponent } from './checkout/checkout.component';
     HomeComponent,
     ProductsComponent,
     ShoppingCartComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    MyOrdersComponent,
+    OrderSuccessComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {
+        path:'',component:HomeComponent
+      },
+      {
+        path:'products',component:ProductsComponent
+      },
+      {
+        path:'shopping-cart',component:ShoppingCartComponent
+      },
+      {
+        path:'checkout',component:CheckoutComponent
+      },
+      {
+        path:'order-successs',component:OrderSuccessComponent
+
+      },
+      {
+        path:'login',component:LoginComponent
+      },
+      {
+        path:'admin/orders',component:AdminOrdersComponent
+      },
+      {
+        path:'admin/products',component:AdminProductsComponent
+      },
+      {
+        path:'orders',component:MyOrdersComponent
+      }
+    ])
     
   ],
   providers: [],
